1d54ff02021a70f88c3f7d0328358da84e489482
Eunice-ai <eunyben4@gmail.com>
2022-10-17 14:35:16 -0700

    tasks 6 Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry
+ * Description: Print all single digit number
+ * of base 10, starting from 0,
+ * Return: Success 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
