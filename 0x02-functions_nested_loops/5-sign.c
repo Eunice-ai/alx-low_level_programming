022a5b691cea67bcf38e399ffd84727a4cbee5cf
Eunice-ai <eunyben4@gmail.com>
2022-10-18 21:31:12 -0700

    Tasks 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - function
+ *
+ * @n: number to check
+ * Return: true (0)) or false (1) or (-1);
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar("0");
+		return (0);
+	}
+	else
+	{
+		_putchar('_');
+		return (-1);
+	}
+}
