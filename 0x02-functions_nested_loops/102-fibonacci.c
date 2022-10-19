9c0c47e51060744e4c5644c72417552dc574e440
Eunice-ai <eunyben4@gmail.com>
2022-10-19 03:36:55 -0700

    Tasks 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the first 50 fibonacci numbers
+ * starting with 1 and 2, followed by a new line
+ * Return: Success (0)
+ */
+int main(void)
+{
+	long int a, b, sum;
+	int z;
+
+	sum = 0;
+	a = 1;
+	b = 0;
+
+	for (z = 0; z < 50; z++)
+	{
+		b = a;
+		a += sum;
+		if (a == 49)
+		{
+			printf("%ld\n", a);
+		}
+		else
+		{
+			printf("%ld, ", a);
+		}
+	}
+	return (0);
+}
