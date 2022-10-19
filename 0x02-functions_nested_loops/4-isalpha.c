c018b9ad56720b995e96bccc4bdbd7173d6561fc
Eunice-ai <eunyben4@gmail.com>
2022-10-18 21:23:17 -0700

    Tasks 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - function name
+ * Description: check for alphabet
+ * @c: character to check
+ * Return: true (0) or false (1);
+ */
+
+int _isalpha(int c)
+{
+	char a = c >= 'a' && c <= 'z';
+	char b =  c >= 'A' && c <= 'Z';
+
+	if (a || b)
+		return (1);
+	else
+		return (0);
+}
