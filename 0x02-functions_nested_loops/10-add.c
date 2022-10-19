0e9e4208994a6c343204f972d4b97a9974b5614a
Eunice-ai <eunyben4@gmail.com>
2022-10-19 00:06:03 -0700

    Tasks 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Add integers and return result
+ * @a: first number
+ * @b: second number
+ * Return: Always 0
+ */
+
+int add(int a, int b);
+{
+	return (a + b);
+}
+
