20edbe3004e2fb7dda83f61999fa76c083257ff0
Eunice-ai <eunyben4@gmail.com>
2022-10-18 21:40:43 -0700

    Tasks 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,2 +1,15 @@
 #include "main.h"
 
+/**
+ * _abs - function
+ *
+ * @n: number to convert
+ * Return: true (0) or false (1) or (-1)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	else
+		return (n);
+}
