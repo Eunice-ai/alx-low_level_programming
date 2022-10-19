6e351a3a81ee46db1d10c2b3316c7f415a6b35a1
Eunice-ai <eunyben4@gmail.com>
2022-10-19 01:42:46 -0700

    Tasks 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - function
+ * Description: prints the n times table,
+ * starting with 0
+ * @n: max number
+ * Return: true (0)
+ */
+void print_times_table(int n)
+{
+	int w, x, y, z, op;
+
+	for (w = 0; w <= n; w++)
+	{
+		if (n < 0 || n > 15)
+		{
+			break;
+		}
+		_putchar(48);
+		for (x = 1; x <= n; x++)
+		{
+			op = w * x;
+			_putchar(44);
+			_putchar(32);
+			_putchar(op + 48);
+			if (op <= 9)
+			{
+				_putchar(32);
+				_putchar(32);
+				_putchar(op + 48);
+			}
+			else if (op > 9 && op < 100)
+			{
+				_putchar(32);
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+			else
+			{
+				y = op / 10;
+				z = y / 10;
+				_putchar(z + 48);
+				_putchar((y % 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
